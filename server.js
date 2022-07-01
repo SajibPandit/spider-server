93be937e0804fdb9e0248fe12ef6252ac4fb3e2d
Sajib Pandit <sajibpandit1999@gmail.com>
2022-07-01 18:02:25 +0600

    category click sort implementeyfggd

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const server = app.listen(PORT, () => {
 
 //configuring socket server
 const io = require('socket.io')(server, {
-  pingTimeout: 60000,
+  // pingTimeout: 60000,
   cors:"*"
 });
 
