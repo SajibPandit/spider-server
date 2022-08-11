1f6f5f9ab615173088dc84ec9a155be83f728cc3
Sajib Pandit <sajibpandit1999@gmail.com>
2022-08-11 22:11:03 +0600

    category icon field problem solved

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -60,6 +60,7 @@ exports.getAllRootCategories = catchAsync(async (req, res, next) => {
             _id: root.id,
             name: root.name,
             slug: root.slug,
+            icon: cate.icon,
             createdAt: root.createdAt,
             childrens: getChildCategories(root.id, category),
           });
